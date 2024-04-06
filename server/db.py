4fb701f59e3e0c1bb8237a8d3809e94dcc57c0e3
jan <janlemcke@live.de>
2024-04-06 14:41:16 +0200

    Better seperation + chat interface between client / server.

--- server/db.py
+++ server/db.py
@@ -1,7 +1,8 @@
 import os
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.pgvector import PGVector
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import pypdfium2 as pdfium
 
 class VectorStore():
     COLLECTION_NAME = "test_collection"
@@ -19,6 +20,10 @@ class VectorStore():
     encode_kwargs = {'normalize_embeddings': False}
 
     def __init__(self):
+        # chek if on mac or windows
+        if os.name == 'posix':
+            self.model_kwargs = {'device': 'cpu'}
+
         self.embedding_model = HuggingFaceEmbeddings(
             model_name=self.model_name,
             model_kwargs=self.model_kwargs,
@@ -31,3 +36,26 @@ class VectorStore():
             embedding_function=self.embedding_model,
             pre_delete_collection=True,
         )
+
+    def get_store(self):
+        return self.store
+
+    def injest_files(self, files):
+        pdf_text = self.get_pdf_text(files)
+        text_chunks = self.get_text_chunks(pdf_text)
+        self.store.add_texts(text_chunks)
+
+    def get_text_chunks(self, text):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=50)
+        chunks = text_splitter.split_text(text)
+        return chunks
+
+    def get_pdf_text(self, pdf_docs):
+        text = ""
+        for pdf in pdf_docs:
+            pdf_reader = pdfium.PdfDocument(pdf)
+            for i in range(len(pdf_reader)):
+                page = pdf_reader.get_page(i)
+                textpage = page.get_textpage()
+                text += textpage.get_text_bounded() + "\n"
+        return text
--- server/main.py
+++ server/main.py
@@ -1,7 +1,43 @@
 import os
+import time
+
 from fastapi import FastAPI, UploadFile, File, Response
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain_community.llms.llamacpp import LlamaCpp
+
+from db import VectorStore
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup_event():
+    model_path = "./models/tinyllama-1.1b-chat-v0.3.Q2_K.gguf"
+    print("Loading LlamaCpp model...")
+    start = time.time()
+    app.state.llm = LlamaCpp(
+        model_path=model_path,
+        temperature=0.5,
+        verbose=False,
+        n_ctx=2048,
+    )
+    end = time.time()
+    print(f"LlamaCpp model loaded in {end - start} seconds")
+
+    print("Loading VectorStore...")
+    start = time.time()
+    app.state.vectorstore = VectorStore()
+    end = time.time()
+    print(f"VectorStore loaded in {end - start} seconds")
+
+    app.state.chain = ConversationalRetrievalChain.from_llm(
+        llm=app.state.llm,
+        retriever=app.state.vectorstore.get_store().as_retriever(search_k=5),
+        memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True),
+        # return_source_documents=True,
+    )
+
 
 @app.post("/upload-file/")
 async def upload_file(file: UploadFile = File(...)):
@@ -12,7 +48,14 @@ async def upload_file(file: UploadFile = File(...)):
         # Save the uploaded file to the "uploads" directory with the original name
         with open(os.path.join("uploads", file.filename), "wb") as buffer:
             buffer.write(await file.read())
+        app.state.vectorstore.injest_files(files=["uploads/" + file.filename])
 
         return Response(content=f"File {file.filename} uploaded successfully", status_code=200)
     except Exception as e:
         return Response(content=f"Failed to upload file: {e}", status_code=500)
+
+
+@app.get("/chat/")
+async def chat(query: str):
+    response = app.state.chain(query)
+    return response["answer"]
