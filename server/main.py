88be07954570c5cb27702a06ba865aef396598d3
jarek <jarek@bccn-berlin.de>
2024-05-13 16:46:17 +0200

    Using in-memory model

--- server/main.py
+++ server/main.py
@@ -1,23 +1,22 @@
 import io
 import json
 import os
+import time
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, File, Response, UploadFile
+from langchain import ConversationalRetrievalChain
+from langchain.memory.buffer import ConversationBufferMemory
 from langchain_community.llms.llamacpp import LlamaCpp
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from pypdf import PdfReader, PdfWriter
 
-app = FastAPI()
+from .db import VectorStore
 
-with open("PROMPT.txt", "r") as f:
-    PROMPT = f.read()
 
-with open("context.json", "r") as f:
-    context = json.loads(f.read())
-
-@app.on_event("startup")
-def startup_event():
+@asynccontextmanager
+async def lifespan():
     model_path = "./models/tinyllama-1.1b-chat-v0.3.Q2_K.gguf"
     print("Loading LlamaCpp model...")
     start = time.time()
@@ -36,12 +35,16 @@ def startup_event():
     end = time.time()
     print(f"VectorStore loaded in {end - start} seconds")
 
-    app.state.chain = ConversationalRetrievalChain.from_llm(
-        llm=app.state.llm,
-        retriever=app.state.vectorstore.get_store().as_retriever(search_k=5),
-        memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True),
-        # return_source_documents=True,
-    )
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+with open("PROMPT.txt", "r") as f:
+    PROMPT = f.read()
+
+with open("context.json", "r") as f:
+    context = json.loads(f.read())
 
 
 @app.post("/upload-file/")
@@ -68,30 +71,15 @@ async def fill_pdf(file: UploadFile = File(...), context: dict = {}):
     reader = PdfReader(bio)
     fields = reader.get_fields()
 
-    # prompt = ChatPromptTemplate.from_template(PROMPT)
-    # model = LlamaCpp(
-    #     model_path="models/open-llama-3b-v2-wizard-evol-instuct-v2-196k.Q4_K_M.gguf",
-    #     temperature=0.5,
-    #     verbose=True,
-    #     n_ctx=2048,
-    #     n_batch=256,
-    #     n_gpu_layers=-1,
-    # )
-    # from langchain_anthropic import ChatAnthropic
-    # model = ChatAnthropic(model="claude-3-opus-20240229")
+    prompt = ChatPromptTemplate.from_template(PROMPT)
+    output = StrOutputParser()
+    chain = prompt | app.state.llm | output
 
-    # output = StrOutputParser()
-    # chain = prompt | model | output
-
-    # result = chain.invoke(
-    #     {
-    #         "fields": {name: field.field_type for name, field in fields.items()},
-    #         "context": json.dumps(context),
-    #     }
-    # )
-
-    # This is what claude gave me.
-    result = r"""{"Name des Betreuers": "Dr. Hans Mustermann", "Adresse des Betreuers": null, "Titel der Lab Rotation": null, "BCCNBetreuer für externe Lab Rotations": null, "Angestrebter Zeitraum": "01.01.2023 - 31.12.2023", "Titel der Lab Rotation_2": "Erlernen von Python", "Ort  location": null, "Datum  date": null, "Check Box1": null, "Check Box3": null, "Check Box4": null, "Check Box5": null, "Check Box7": null, "Check Box8": null, "Matrikelnummer": null, "Name des Studierenden": "Herbert Basti"}"""
+    data = {
+        "fields": {name: field.field_type for name, field in fields.items()},
+        "context": json.dumps(context),
+    }
+    result = chain.invoke(data)
 
     result_json = json.loads(result)
     writer = PdfWriter()
@@ -109,7 +97,16 @@ async def fill_pdf(file: UploadFile = File(...), context: dict = {}):
     headers = {"Content-Disposition": f"attachment; filename={filled_filename}"}
     return Response(filled_pdf, media_type="application/pdf", headers=headers)
 
+
 @app.get("/chat/")
 async def chat(query: str):
-    response = app.state.chain(query)
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=app.state.llm,
+        retriever=app.state.vectorstore.get_store().as_retriever(search_k=5),
+        memory=ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        ),
+        # return_source_documents=True,
+    )
+    response = chain(query)
     return response["answer"]
